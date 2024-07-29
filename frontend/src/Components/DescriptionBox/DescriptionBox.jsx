519c8fcd78018eb44fb37bbfc005da4e5a9de4e0
meetchauhan1 <meetchauhan240@gmail.com>
2024-07-29 19:28:30 +0530

    E-commerce

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './DescriptionBox.css'
+export const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+        <div className="descriptionbox-navigator">
+            <div className="descriptionbox-nav-box">Description</div>
+            <div className="descriptionbox-nav-box fade">Reviews(122)</div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>An e-commerce website is on online platform that locilitates the buying ond selling of products of services over the Partiseres de virtual marketgance where Danses and customers,nd conduct transactions without the need for a physical presence. E-commerce watisites have joined memena popustunty oue to the core accoyond the gcommerce websites typically display products or services slang with detailed descriptions, images, prices and any</p>
+        </div>
+    </div>
+    
+  )
+}
---
+++ frontend/src/Components/NewsLetter/NewsLetter.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './NewsLetter.css'
+export const NewsLetter = () => {
+  return (
+    <div className='newsletters'>
+        <h1>Get EXclusive Offers on your Email</h1>
+        
+        <p>Subscribe to our newletter and stay updated</p>
+        <div>
+            <input type="email" placeholder='Your Email Id' />
+            <button>Subscribe</button>
+        </div>
+    </div>
+  )
+}
